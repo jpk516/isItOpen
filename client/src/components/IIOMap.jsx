1eaea150abb990cc6978eb1346dec2041e6543c4
JackC20 <114776880+JackC20@users.noreply.github.com>
2024-03-01 13:48:59 -0600

    Update IIOMap.jsx
    Add Map Styling and change latitude and longitude to be over Columbia as default

--- client/src/components/IIOMap.jsx
+++ client/src/components/IIOMap.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import {APIProvider, Map} from '@vis.gl/react-google-maps';
+import {APIProvider, Map, Marker} from '@vis.gl/react-google-maps';
+import mapStyles from '../styles/mapStyles'; //import style sheet for the map
 
 function IIOMap() {
     return (
-      <APIProvider apiKey="AIzaSyCPKB7QwX25zmCWHLt8InMcMsQ4B9_RCNQ">
+      <APIProvider apiKey="AIzaSyDBsAc39kykWMw9GcY0ReFazPl1DY4XRbg">
               <Map
-                defaultCenter={{lat: 22.54992, lng: 0}}
-                defaultZoom={3}
+                defaultCenter={{lat: 38.9517, lng: -92.3341}} //Set latitude and longitude to Columbia Missouri
+                defaultZoom={15}
                 style={{height: '600px' }}
                 gestureHandling={'greedy'}
                 disableDefaultUI={true}
-              />
+                options={{styles:mapStyles}} //add styling to the map from the mapStyles.js file
+                
+              >
+              <Marker position={{lat: 38.9506, lng: -92.3268}}></Marker>
+              </Map>
         </APIProvider>
     );
   }
   
   export default IIOMap;
-
