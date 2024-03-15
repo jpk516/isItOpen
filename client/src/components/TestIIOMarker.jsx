f474a9ac3ad6ac84444499e3d3eefa6fec88dda0
JackC20 <114776880+JackC20@users.noreply.github.com>
2024-03-15 12:56:49 -0500

    Update TestIIOMarker.jsx
    Update Map markers

--- client/src/components/TestIIOMarker.jsx
+++ client/src/components/TestIIOMarker.jsx
@@ -1,34 +1,55 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   AdvancedMarker,
+  Marker,
   InfoWindow,
   useAdvancedMarkerRef
 } from '@vis.gl/react-google-maps';
 
-function IIOMarker({venue}) {
+// Define the icons for each type of venue
+const icons = {
+  Bar: {
+    icon: "https://img.icons8.com/plasticine/45/beer.png",
+  },
+  Coffee: {
+    icon: "https://img.icons8.com/color/45/coffee-to-go.png",
+  },
+  Restaurant: {
+    icon: "https://img.icons8.com/matisse/45/restaurant.png",
+  },
+};
+
+function IIOMarker({ venue }) {
   const [infowindowOpen, setInfowindowOpen] = useState(false);
   const [markerRef, marker] = useAdvancedMarkerRef();
 
   return (
     <>
-      <AdvancedMarker
+
+      <Marker //using Marker, to use advanced marker you just do <AdvancedMarker
         ref={markerRef}
         onClick={() => setInfowindowOpen(true)}
-        position={{lat: venue.geo.coordinates[1], lng: venue.geo.coordinates[0]}}
+        position={{ lat: venue.geo.coordinates[1], lng: venue.geo.coordinates[0] }}
         title={venue.name}
+        icon={icons[venue.type]?.icon || ''}  // checks if the icon exists in the icons dictionary
       />
       {infowindowOpen && (
         <InfoWindow
           anchor={marker}
           maxWidth={200}
-          onCloseClick={() => setInfowindowOpen(false)}>
-          This is an example for the{' '}
-          <code style={{whiteSpace: 'nowrap'}}>&lt;AdvancedMarker /&gt;</code>{' '}
-          combined with an Infowindow.
+          onCloseClick={() => setInfowindowOpen(false)}
+        >
+        <div className="info-window-content">
+        <h3>{venue.name}</h3>
+        <p>Serves: {venue.type}</p>
+        <p className="status-open">Status: Open</p>
+        </div>
+
         </InfoWindow>
-      )}
+    )}
+
     </>
   );
-};
+}
 
 export default IIOMarker;
