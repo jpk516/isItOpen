4ee0db56c6a25041cf3368707f50f9c77e1c64c5
Gerard Lucas <gerard.lucas@conperio.com>
2024-03-25 00:55:45 -0500

    open menu below icon

--- client/src/components/TopNav.jsx
+++ client/src/components/TopNav.jsx
@@ -138,15 +138,7 @@ function TopNav({ authenticated, onAuthChange, onThemeChange, username }) {
               <Menu
                 id="menu-appbar"
                 anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
                 keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
                 open={Boolean(anchorEl)}
                 onClose={handleClose}
               >
