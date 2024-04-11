19c8cecd8d492147416f312b2bc7b75d96c1a570
Gerard Lucas <gerard.lucas@conperio.com>
2024-04-10 22:54:23 -0500

    add open/closed to check in cards

--- client/src/components/CheckInList.jsx
+++ client/src/components/CheckInList.jsx
@@ -8,6 +8,7 @@ import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 import ThumbDownAltIcon from '@mui/icons-material/ThumbDownAlt';
+import Alert from '@mui/material/Alert';
 import { useNavigate } from 'react-router-dom';
 import MuiLink from '@mui/material/Link';
 import { Link } from 'react-router-dom';
@@ -22,7 +23,6 @@ function CheckInList({ checkIns, isVenuePage = false }) {
     if (!checkIns) return null;
     checkIns.sort((a, b) => new Date(b.created) - new Date(a.created));
 
-    // Placeholder functions for voting
     const handleUpvote = (checkInId) => {
         console.log('Upvoting', checkInId);
         // Increment vote logic here
@@ -33,7 +33,6 @@ function CheckInList({ checkIns, isVenuePage = false }) {
         // Decrement vote logic here
     };
 
-    // Function to count votes
     const countVotes = (votes, type) => votes.filter(vote => vote.vote === type).length;
 
     return (
@@ -45,44 +44,47 @@ function CheckInList({ checkIns, isVenuePage = false }) {
                 return (
                     <Grid item xs={12} md={4} key={checkIn._id}>
                         <Card variant="outlined">
-                            <Box>
-                                <CardContent>
-                                    <Typography variant="subtitle2" color="primary" gutterBottom>
-                                        {checkIn.venue.type}
+                            {checkIn.open ? (
+                                <Alert severity="success">Open</Alert>
+                            ) : (
+                                <Alert severity="error">Closed</Alert>
+                            )}
+                            <CardContent>
+                                <Typography variant="subtitle2" color="primary" gutterBottom>
+                                    {checkIn.venue.type}
+                                </Typography>
+                                <Typography variant="h6" component="div">
+                                    {checkIn.venue.name}
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    {new Date(checkIn.created).toLocaleString()}
+                                </Typography>
+                                <div>
+                                    {checkIn.tags.map((tag, index) => (
+                                        <Chip label={tag} color="secondary" size="small" key={index} sx={{ m: .5 }} />
+                                    ))}
+                                </div>
+                                {checkIn.comment && (
+                                    <Typography variant="body2" mt={2}>
+                                        {checkIn.comment}
                                     </Typography>
-                                    <Typography variant="h6" component="div">
-                                        {checkIn.venue.name}
-                                    </Typography>
-                                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                        {new Date(checkIn.created).toLocaleString()}
-                                    </Typography>
-                                    <div>
-                                        {checkIn.tags.map((tag, index) => (
-                                            <Chip label={tag} color="secondary" size="small" key={index} sx={{ m: .5 }} />
-                                        ))}
-                                    </div>
-                                    {checkIn.comment && (
-                                        <Typography variant="body2" mt={2}>
-                                            {checkIn.comment}
-                                        </Typography>
-                                    )}
-                                </CardContent>
-                                <Box p={2} display="flex" justifyContent="space-between" alignItems="center">
-                                    { !isVenuePage &&   
+                                )}
+                            </CardContent>
+                            <Box p={2} display="flex" justifyContent="space-between" alignItems="center">
+                                {!isVenuePage && (
                                     <MuiLink component={Link} to={`/venues/${checkIn.venue.name}`} color="primary">
                                         View Venue
                                     </MuiLink>
-                                    }
-                                    <Box>
-                                        <IconButton onClick={() => handleUpvote(checkIn._id)} color="primary">
-                                            <ThumbUpAltIcon />
-                                            <Typography variant="body2" sx={{ ml: 1 }}>{upVotes}</Typography>
-                                        </IconButton>
-                                        <IconButton onClick={() => handleDownvote(checkIn._id)} color="secondary">
-                                            <ThumbDownAltIcon />
-                                            <Typography variant="body2" sx={{ ml: 1 }}>{downVotes}</Typography>
-                                        </IconButton>
-                                    </Box>
+                                )}
+                                <Box>
+                                    <IconButton onClick={() => handleUpvote(checkIn._id)} color="primary">
+                                        <ThumbUpAltIcon />
+                                        <Typography variant="body2" sx={{ ml: 1 }}>{upVotes}</Typography>
+                                    </IconButton>
+                                    <IconButton onClick={() => handleDownvote(checkIn._id)} color="secondary">
+                                        <ThumbDownAltIcon />
+                                        <Typography variant="body2" sx={{ ml: 1 }}>{downVotes}</Typography>
+                                    </IconButton>
                                 </Box>
                             </Box>
                         </Card>
