396aa3abe605ce15af25b8481b456f8bb321de45
Gerard Lucas <gerard.lucas@conperio.com>
2024-04-13 22:45:57 -0500

    implement snackbar at app level

--- client/src/contexts/AppContext.jsx
+++ client/src/contexts/AppContext.jsx
@@ -1,18 +1,30 @@
 import { createContext, useContext, useState } from 'react';
+import ResultSnack from '../components/ResultSnack';
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
     const [auth, setAuth] = useState(null);
+    const [showSnackbar, setShowSnackbar] = useState(false);
+    const [snackbarMessage, setSnackbarMessage] = useState('');
+    const [snackbarSeverity, setSnackbarSeverity] = useState('info');
+    
+    const toggleSnackbar = (message, severity) => {
+      setSnackbarMessage(message);
+      setSnackbarSeverity(severity);
+      setShowSnackbar(show => !show);
+    }
     
     const value = {
       auth,
       setAuth,
+      toggleSnackbar,
     };
   
     return (
       <AppContext.Provider value={value}>
         {children}
+        <ResultSnack open={showSnackbar} severity={snackbarSeverity} message={snackbarMessage} onClose={() => setShowSnackbar(false)} />
       </AppContext.Provider>
     );
   };
